2efc17d0471bc81f6d4a98ace29bf8eb4ceb76f9
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2020-05-31 20:11:37 +0900

    Add gitconfig

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # add submodule
 git submodule update --init --recursive
 
+# homebrew
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew bundle
+
 # symlink dotfiles
-ln -s ~/tadashi0713/dotfiles/.zshrc ~/.zshrc
-ln -s ~/tadashi0713/dotfiles/.hyper.js ~/.hyper.js
+ln -s ~/tadashi0713/dotfiles/.zshrc ~/.zshrc 2>/dev/null
+ln -s ~/tadashi0713/dotfiles/.hyper.js ~/.hyper.js 2>/dev/null
+ln -s ~/tadashi0713/dotfiles/.gitconfig ~/.gitconfig 2>/dev/null
