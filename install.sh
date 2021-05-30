633495557bf187fee7327438dc1781dccaf11eeb
Tadashi Nemoto <tadashi.nemoto0713@gmail.com>
2021-05-30 14:45:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-# add submodule
-git submodule update --init --recursive
-
-# homebrew
-which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew bundle
 
 # symlink dotfiles
